9d5e5c3689eeb4e6962382405a37dfedb7a7832e
Sonveer Singh <sonveer.s@icreon.com>
2019-11-29 00:25:26 +0530

    first commit

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
+
+export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.changeHandler = this.changeHandler.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    changeHandler = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date,
+        };
+
+        console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add/', exercise)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                            required
+                            className="form-control"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.changeHandler}>
+                            {
+                                this.state.users.map(function (user) {
+                                    return <option
+                                        key={user}
+                                        value={user}>{user}
+                                    </option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            name="description"
+                            value={this.state.description}
+                            onChange={this.changeHandler}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration (in minutes): </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="duration"
+                            value={this.state.duration}
+                            onChange={this.changeHandler}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                name="date"
+                                onChange={this.changeHandler}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
